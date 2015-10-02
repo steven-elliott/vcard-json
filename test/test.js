2a84e6e84eb7206b34769dc05655be66cc6abffd
Steven Elliott <steven.patrick.elliott@gmail.com>
2015-10-01 22:25:14 -0700

    fixing bug with whitespace

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
 var vcard = require('../lib/vcard-json');
 
-vcard.parseFile('./Apple Inc..vcf', function(err, data){
+vcard.parseFile('./some-contact.vcf', function(err, data){
+  
   if(err) console.log('oops:'+ err);
   else {
-    console.log('should be good to go:\n'+ JSON.stringify(data));    
+    console.log('should be good to go:\n'+ JSON.stringify(data)); 
   }
 });
